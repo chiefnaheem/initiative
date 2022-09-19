1138984b57d50f533fb28577072f02431a350f60
naheem adedokun <nadedokun@heckerbella.com>
2022-09-19 20:36:14 +0100

    first commit

---
+++ src/routes/auth.ts
@@ -0,0 +1,17 @@
+import express from "express";
+const router = express.Router();
+import {
+  register,
+  login,
+} from "../controller/authContoller";
+import protectRoute from "../middleware/authenticate";
+
+router.route("/register").post(register);
+router.route("/login").post(login);
+router.route("/verify/:token").get(confirmEmail);
+router.route("/logout").post(logout);
+router.post("/changepassword", protectRoute, changePassword);
+router.route("/forgotpassword").post(forgotPassword);
+router.route("/resetpassword/:token").post(resetPassword);
+
+export default router;
